742305904f28e8723124df1df09a347a508043d6
marvax <33978801+m4rv4x@users.noreply.github.com>
2023-07-07 19:10:48 +0200

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,16 +1,45 @@
 import pytube
 import json
+import os
 
-playlist_url = input("Enter the playlist URL: ")
-playlist = pytube.Playlist(playlist_url)
+while True:
+    try:
+        playlist_url = input("Enter the playlist URL: ")
+        playlist = pytube.Playlist(playlist_url)
 
-youtube_jingles = {}
+        youtube_songs = {}
 
-for video_url in playlist.video_urls:
-    video = pytube.YouTube(video_url)
-    video_info = {
-        video.title: video_url,
-    }
-    youtube_jingles.update(video_info)
+        for video_url in playlist.video_urls:
+            try:
+                video = pytube.YouTube(video_url)
+                video_info = {
+                    video.title: video_url,
+                }
+                youtube_songs.update(video_info)
+            except Exception as e:
+                print(f"Error processing video: {video_url}")
+                print(f"Error message: {str(e)}")
 
-print(json.dumps(youtube_jingles, indent=4))
+        playlist_title = playlist.title
+        log_folder = "logs"
+        if not os.path.exists(log_folder):
+            os.makedirs(log_folder)
+        log_file = f"{log_folder}/{playlist_title}.json"  # Save logs in /logs/ directory
+
+        if os.path.exists(log_file):
+            replace = input("Playlist already exists. Do you want to replace it? (y/n): ")
+            if replace.lower() == "y":
+                with open(log_file, 'w') as f:
+                    json.dump(youtube_songs, f, indent=4)
+                print(json.dumps(youtube_songs, indent=4))
+                print(f"{len(youtube_songs)} results fetched saved in {log_folder}/{playlist_title}.json")  # Print the number of results fetched
+            else:
+                print("Playlist not saved.")
+        else:
+            with open(log_file, 'w') as f:
+                json.dump(youtube_songs, f, indent=4)
+            print(json.dumps(youtube_songs, indent=4))
+            print(f"{len(youtube_songs)} results fetched saved in {log_folder}/{playlist_title}.json")  # Print the number of results fetched
+    except Exception as e:
+        print(f"Error processing playlist: {playlist_url}")
+        print(f"Error message: {str(e)}")
